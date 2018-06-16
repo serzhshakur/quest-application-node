651bb5c60b1e46d28a24c8774a801395d3c01934
Sergey Shakur <sergey.shakur@gmail.com>
2018-06-16 20:28:21 +0300

    added create and update time for session to be able to display quests execution time

--- src/dbUtils.js
+++ src/dbUtils.js
@@ -28,15 +28,15 @@ module.exports.queryQuestFinalWords = (db, questId) => {
 module.exports.questExists = (db, questId) => {
     return db.collection('questions')
         .find({ 'id': questId }).count()
-        .catch(err => console.error("error while counting quests"))
+        .catch(err => console.error("error while counting quests\n", err))
         .then(count => count > 0)
 }
 
 module.exports.querySessionInfo = (db, sessionId) => {
     return session = db.collection('sessions')
         .find({ '_id': new ObjectID(sessionId) })
-        .project({ _id: false }).toArray()
-        .catch(err => console.error(`Error while trying to get question info for session id ${sessionId}`))
+        .project({ _id: 0 }).toArray()
+        .catch(err => console.error(`Error while trying to get question info for session id ${sessionId}\n`, err))
         .then(array => array[0]);
 }
 
@@ -47,15 +47,24 @@ module.exports.createSessionAndGetId = (db, questId) => {
                 'questId': questId,
                 'questionIndex': 0,
                 'wrongAnswers': 0,
-                'hintRetrievals': 0
+                'hintRetrievals': 0,
+                "created": new Date(),
+                "updated": new Date()
             })
-        .catch(err => console.error(`Error while creating session for quest id ${questId}`))
+        .catch(err => console.error(`Error while creating session for quest id ${questId}\n`, err))
         .then((r) => r.insertedId.toString())
 }
 
 module.exports.updateSession = (db, sessionId, newValues) => {
     return db.collection('sessions')
-        .updateOne({ '_id': new ObjectID(sessionId) }, { "$set": newValues })
+        .updateOne({ '_id': new ObjectID(sessionId) },
+            {
+                "$set": newValues,
+                '$currentDate': {
+                    'updated': true
+                }
+            }
+        )
         .catch(err => console.error(`Error updating session ${sessionId}: `, err))
         .then(() => console.log(`successfully updated a session ${sessionId} with values ${JSON.stringify(newValues)}`));
 }
@@ -66,13 +75,13 @@ module.exports.queryQuests = db => {
     return db.collection('questions')
         .find({})
         .project({ _id: false }).toArray()
-        .catch(err => console.error(`Error while quering quests: \n${e}`));
+        .catch(err => console.error("Error while querying quests:\n", err));
 }
 
 module.exports.createQuest = (db, quest) => {
     return db.collection('questions')
         .insertOne(quest)
-        .catch(err => console.error(`Error while creating quest ${quest}`))
+        .catch(err => console.error(`Error while creating quest ${quest}`, err))
 }
 
 module.exports.deleteQuest = (db, questId) => {
@@ -83,12 +92,6 @@ module.exports.deleteQuest = (db, questId) => {
 module.exports.deleteSessionsForQuest = (db, questId) => {
     return db.collection('questions')
         .deleteMany({ questId })
-}
-
-module.exports.deleteQuest = (db, questId) => {
-    return db.collection('questions')
-        .deleteOne(questId)
-        .catch(err => console.error(`Unable to delete quest with id ${questId}`))
 }
 
 module.exports.updateQuest = (db, questId, newValues) => {
